e26115260df1a798ed8c82d7191b5db682676fec
Gökhan Çavdar <gokhan.cavdarrr@gmail.com>
2022-04-26 18:54:37 +0300

    Cookie policy eklendi

--- home/app.js
+++ home/app.js
@@ -104,3 +104,24 @@ function showPass() {
         temp.type = "password";
     }
 }
+
+
+
+
+const cookieContainer = document.querySelector(".cookie-container");
+const cookieButton = document.querySelector(".cookie-btn");
+
+cookieButton.addEventListener("click", () => {
+  cookieContainer.classList.remove("active");
+  localStorage.setItem("cookieBannerDisplayed", "true");
+});
+
+setTimeout(() => {
+  if (!localStorage.getItem("cookieBannerDisplayed")) {
+    cookieContainer.classList.add("active");
+  }
+}, 10000);
+
+function closeDialog() {
+  cookieContainer.style.display = "none"
+}
